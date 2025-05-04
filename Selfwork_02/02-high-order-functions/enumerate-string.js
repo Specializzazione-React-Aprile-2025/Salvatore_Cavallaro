694de6499cb1171eea95cd31d07a7c9a57d50023
SalvatoreCavallaro <tore1793@gmail.com>
2025-05-04 19:59:31 +0200

    enumerate-string function work fine

---
+++ Selfwork_02/02-high-order-functions/enumerate-string.js
@@ -0,0 +1,10 @@
+/* number([]) => [] number(["a", "b", "c"]) => ["1: a", "2: b", "3: c"] */
+
+
+let number = (array) => {
+    if(array.length == 0) return 'Inserisci un array valido';
+    let i = 1;
+    return array.map( e => e = i++ + ': ' + e);
+}
+
+console.log(number(["a", "b", "c", "d"]));
